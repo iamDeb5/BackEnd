eeb95d1f9cf7af082298630f2b255665a604cadf
Debojyoti <khanradebojyoti@gmail.com>
2026-01-19 15:37:29 +0530

    Backend Day-1

---
+++ Day-1.2/app.js
@@ -0,0 +1,5 @@
+const express = require("express"); // Require Express in our code 
+
+const app = express(); // Server is created for this line
+
+app.listen(3000); // Start this server in port 3000
