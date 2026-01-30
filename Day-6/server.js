77addc227552a9b121385b8537d49c34fd31147b
Debojyoti <khanradebojyoti@gmail.com>
2026-01-30 16:59:02 +0530

    Update .env files of Day-6

--- Day-6/server.js
+++ Day-6/server.js
@@ -1,14 +1,11 @@
+require("dotenv").config();
 const app = require("./src/app");
 const mongoose = require("mongoose");
 
 function connectToDb() {
-    mongoose
-        .connect(
-            "mongodb+srv://khanradebojyoti_db_user:wqZqZe1CPopkvi1s@cluster0.ddzzplb.mongodb.net/day-6",
-        )
-        .then(() => {
-            console.log("Connected to DB");
-        });
+    mongoose.connect(process.env.MONGO_URI).then(() => {
+        console.log("Connected to DB");
+    });
 }
 
 connectToDb();
