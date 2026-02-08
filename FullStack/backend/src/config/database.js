25c7dc808c8ede8f3e7cedde2f78022f94fd05e5
Debojyoti <khanradebojyoti@gmail.com>
2026-02-08 15:21:34 +0530

    error gitignore

--- FullStack/backend/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectToDB = () => {
-    mongoose.connect(process.env.MONGO_URI).then(() => {
-        console.log("Connected To DB");
-    });
-};
-
-module.exports = connectToDB;
