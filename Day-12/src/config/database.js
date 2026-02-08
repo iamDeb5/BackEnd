67924e6504690fb1d9bafffc17644522cc0f908a
Debojyoti <khanradebojyoti@gmail.com>
2026-02-08 15:16:43 +0530

    .gitignore problem occured

--- Day-12/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-function connectToDB() {
-    mongoose.connect(process.env.MONGO_URI).then(() => {
-        console.log("Connected  to DB");
-    });
-}
-
-module.exports = connectToDB;
