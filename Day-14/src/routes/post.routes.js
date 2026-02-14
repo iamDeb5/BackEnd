00428badbaf0df677ec94d39b39262ac3e76cdb0
Debojyoti <khanradebojyoti@gmail.com>
2026-02-15 01:04:52 +0530

    ImageKit Added to post images on server to ImageKit cloud

---
+++ Day-14/src/routes/post.routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const PostController = require("../controllers/post.controller");
+const multer = require("multer");
+const upload = multer({ storage: multer.memoryStorage() });
+
+const postRouter = express.Router();
+
+postRouter.post(
+	"/",
+	upload.single("image"),
+	PostController.createPostController,
+);
+
+module.exports = postRouter;
