cfc5fd623a594a03f2a69f261129630ac6a33591
Debojyoti <khanradebojyoti@gmail.com>
2026-02-15 01:10:05 +0530

    commit test to hide some credentials

--- Day-14/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectToDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI);
-
-    console.log("Connected To Database");
-};
-
-module.exports = connectToDB;
